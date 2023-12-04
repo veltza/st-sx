43eb2b850d5290be424797a9119a4847e144fb18
veltza <106755522+veltza@users.noreply.github.com>
2023-12-04 17:17:29 +0200

    openurlonclick: optimize detection

--- patch/openurlonclick.c
+++ patch/openurlonclick.c
@@ -1,28 +1,26 @@
 int url_x1, url_y1, url_x2, url_y2 = -1;
 int url_draw, url_click, url_maxcol;
 
-static int
-isvalidurlchar(Rune u)
-{
-	/* () and [] can appear in urls, but excluding them here will reduce false
-	 * positives when figuring out where a given url ends. See copyurl patch.
-	 */
-	static char urlchars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		"abcdefghijklmnopqrstuvwxyz"
-		"0123456789-._~:/?#@!$&'*+,;=%";
-	return u < 128 && strchr(urlchars, (int)u) != NULL;
-}
+/* () and [] can appear in urls, but excluding them here will reduce false
+ * positives when figuring out where a given url ends.
+ */
+static char urlchars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	"abcdefghijklmnopqrstuvwxyz"
+	"0123456789-._~:/?#@!$&'*+,;=%";
+
+#define ISVALIDURLCHAR(c)    ((c) < 128 && strchr(urlchars, (int)(c)) != NULL)
 
 /* find the end of the wrapped line */
 static int
 findeowl(Line line)
 {
-	int col = term.col - 1;
+	int i = term.col - 1;
 
 	do {
-		if (line[col].mode & ATTR_WRAP)
-			return col;
-	} while (line[col].u == ' ' && --col >= 0);
+		if (line[i].mode & ATTR_WRAP)
+			return i;
+	} while (!(line[i].mode & ATTR_SET) && --i >= 0);
+
 	return -1;
 }
 
@@ -39,24 +37,23 @@ detecturl(int col, int row, int draw)
 {
 	static char url[2048];
 	Line line;
-	int x1, y1, x2, y2, wrapped;
-	int i = sizeof(url)/2+1, j = sizeof(url)/2-1;
+	int x1, y1, x2, y2;
+	int i = sizeof(url)/2+1, j = sizeof(url)/2;
 	int row_start = row, col_start = col;
-	int minrow = term.scr - term.histf, maxrow = term.scr + term.row - 1;
-
-	if (tisaltscr())
-		minrow = 0, maxrow = term.row - 1;
-	url_maxcol = 0;
+	int minrow = tisaltscr() ? 0 : term.scr - term.histf;
+	int maxrow = tisaltscr() ? term.row - 1 : term.scr + term.row - 1;
 
 	/* clear previously underlined url */
 	if (draw)
 		clearurl();
 
-	if (!isvalidurlchar(TLINE(row)[col].u))
+	url_maxcol = 0;
+	line = TLINE(row);
+
+	if (!ISVALIDURLCHAR(line[col].u))
 		return NULL;
 
 	/* find the first character of url */
-	line = TLINE(row_start);
 	do {
 		x1 = col_start, y1 = row_start;
 		url_maxcol = MAX(url_maxcol, x1);
@@ -66,7 +63,7 @@ detecturl(int col, int row, int draw)
 				break;
 			line = TLINE(row_start);
 		}
-	} while (i > 0 && isvalidurlchar(line[col_start].u));
+	} while (ISVALIDURLCHAR(line[col_start].u) && i > 0);
 
 	/* early detection */
 	if (url[i] != 'h')
@@ -77,28 +74,26 @@ detecturl(int col, int row, int draw)
 	do {
 		x2 = col, y2 = row;
 		url_maxcol = MAX(url_maxcol, x2);
-		url[++j] = line[col].u;
-		wrapped = line[col].mode & ATTR_WRAP;
-		if (++col >= term.col || wrapped) {
-			col = 0;
-			if (++row > maxrow || !wrapped)
+		url[j++] = line[col].u;
+		if (line[col++].mode & ATTR_WRAP) {
+			if (++row > maxrow)
 				break;
+			col = 0;
 			line = TLINE(row);
 		}
-	} while (j < sizeof(url)-2 && isvalidurlchar(line[col].u));
+	} while (col < term.col && ISVALIDURLCHAR(line[col].u) && j < sizeof(url)-1);
 
-	/* Ignore some trailing characters to improve detection. */
-	/* Alacritty and many other terminals also do this. */
-	if (url[j] == ',' || url[j] == '.' || url[j] == ';' || url[j] == ':' ||
-	    url[j] == '?' || url[j] == '!') {
-		x2 = MAX(x2-1, 0);
-		j--;
-	}
-
-	url[++j] = 0;
+	url[j] = 0;
 
 	if (strncmp("https://", &url[i], 8) && strncmp("http://", &url[i], 7))
 		return NULL;
+
+	/* Ignore some trailing characters to improve detection. */
+	/* Alacritty and many other terminals also ignore these. */
+	if (strchr(",.;:?!", (int)(url[j-1])) != NULL) {
+		x2 = MAX(x2-1, 0);
+		url[j-1] = 0;
+	}
 
 	/* underline url (see xdrawglyphfontspecs() in x.c) */
 	if (draw) {
