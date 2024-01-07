1125033017bc054db7a640037e9ba458f560a7aa
veltza <106755522+veltza@users.noreply.github.com>
2024-01-07 16:33:28 +0200

    ligatures: fix emoji modifiers
    Ref.
    https://github.com/cog1to/st-ligatures/issues/36
    https://git.suckless.org/sites/commit/70432622424f20d7132f5965b89127a720d90a1a.html

--- hb.c
+++ hb.c
@@ -89,6 +89,7 @@ void hbtransform(HbTransformData *data, XftFont *xfont, const Glyph *glyphs, int
 
 	hb_buffer_t *buffer = hb_buffer_create();
 	hb_buffer_set_direction(buffer, HB_DIRECTION_LTR);
+	hb_buffer_set_cluster_level(buffer, HB_BUFFER_CLUSTER_LEVEL_MONOTONE_CHARACTERS);
 
 	/* Resize the buffer if required length is larger. */
 	if (hbrunebuffer.capacity < length) {
