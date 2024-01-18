7774fea0a9421c81c6bfdac9f8b1f0b9950a4a86
veltza <106755522+veltza@users.noreply.github.com>
2024-01-18 15:03:00 +0200

    keyboard select: add cursor wrapping and tweak...
    ...the '$' shortcut so that if the cursor is already at the end of the
    text line and the line is wrapped, the cursor moves to the end of the
    next line. And if the cursor is already at the end of the line and the
    line is not wrapped, the cursor moves to the right edge of the screen.

--- patch/keyboardselect_st.c
+++ patch/keyboardselect_st.c
@@ -121,18 +121,30 @@ kbds_copytoclipboard(void)
 }
 
 int
-kbds_ismatch(Line line, int x, int y, int bot, int len)
+kbds_top(void)
+{
+	return IS_SET(MODE_ALTSCREEN) ? 0 : -term.histf + term.scr;
+}
+
+int
+kbds_bot(void)
+{
+	return IS_SET(MODE_ALTSCREEN) ? term.row-1 : term.row-1 + term.scr;
+}
+
+int
+kbds_ismatch(Line line, int x, int y, int len)
 {
 	int i, xo = x, yo = y, hlen = len;
 	Line hline = line;
 
 	if (len <= 0 || (x + kbds_searchlen > len &&
-	    (!(line[len-1].mode & ATTR_WRAP) || y >= bot)))
+	    (!(line[len-1].mode & ATTR_WRAP) || y >= kbds_bot())))
 		return 0;
 
 	for (i = 0; i < kbds_searchlen; i++, x++) {
 		if (x >= len) {
-			if (!(line[len-1].mode & ATTR_WRAP) || ++y > bot)
+			if (!(line[len-1].mode & ATTR_WRAP) || ++y > kbds_bot())
 				return 0;
 			x = 0;
 			line = TLINE(y);
@@ -158,19 +170,17 @@ kbds_ismatch(Line line, int x, int y, int bot, int len)
 int
 kbds_searchall(void)
 {
-	int top = IS_SET(MODE_ALTSCREEN) ? 0 : -term.histf + term.scr;
-	int bot = IS_SET(MODE_ALTSCREEN) ? term.row-1 : term.row + term.scr - 1;
 	int x, y, len, count = 0;
 	Line line;
 
 	if (!kbds_searchlen)
 		return 0;
 
-	for (y = top; y <= bot; y++) {
+	for (y = kbds_top(); y <= kbds_bot(); y++) {
 		line = TLINE(y);
 		len = tlinelen(line);
 		for (x = 0; x < len; x++)
-			count += kbds_ismatch(line, x, y, bot, len);
+			count += kbds_ismatch(line, x, y, len);
 	}
 	tfulldirt();
 
@@ -180,8 +190,6 @@ kbds_searchall(void)
 void
 kbds_searchnext(int dir)
 {
-	int top = IS_SET(MODE_ALTSCREEN) ? 0 : -term.histf + term.scr;
-	int bot = IS_SET(MODE_ALTSCREEN) ? term.row-1 : term.row + term.scr - 1;
 	int xo, yo, x = kbds_c.x, y = kbds_c.y;
 	Line line = TLINE(y);
 	int len = tlinelen(line);
@@ -198,24 +206,22 @@ kbds_searchnext(int dir)
 			x += dir;
 			if (x < 0 || x >= len) {
 				y += dir;
-				if (y < top)
-					y = bot, wrapped++;
-				else if (y > bot)
-					y = top, wrapped++;
+				if (y < kbds_top())
+					y = kbds_bot(), wrapped++;
+				else if (y > kbds_bot())
+					y = kbds_top(), wrapped++;
 				if (wrapped > 1)
 					goto end;
 				line = TLINE(y);
 				len = tlinelen(line);
 				x = (dir > 0) ? 0 : MAX(len-1, 0);
 			}
-			if (kbds_ismatch(line, x, y, bot, len)) {
+			if (kbds_ismatch(line, x, y, len)) {
 				if (y < 0)
 					kscrollup(&((Arg){ .i = -y }));
 				else if (y >= term.row)
 					kscrolldown(&((Arg){ .i = y - term.row + 1 }));
 				LIMIT(y, 0, term.row-1);
-				if (!IS_SET(MODE_ALTSCREEN))
-					top = -term.histf + term.scr, bot =  term.row + term.scr - 1;
 				kbds_c.x = x, kbds_c.y = y;
 				kbds_quant--;
 				break;
@@ -249,14 +255,13 @@ kbds_isdelim(Line line, int x, int y, int xoff, int len, wchar_t *delims)
 	for (;;) {
 		x += xoff;
 		if (x >= len) {
-			if (len <= 0 || !(line[len-1].mode & ATTR_WRAP) ||
-			   ++y >= (IS_SET(MODE_ALTSCREEN) ? term.row : term.row + term.scr))
+			if (len <= 0 || !(line[len-1].mode & ATTR_WRAP) || ++y > kbds_bot())
 				return 1;
 			line = TLINE(y);
 			len = tlinelen(line);
 			x = 0;
 		} else if (x < 0) {
-			if (--y < (IS_SET(MODE_ALTSCREEN) ? 0 : -term.histf + term.scr))
+			if (--y < kbds_top())
 				return 1;
 			line = TLINE(y);
 			len = tlinelen(line);
@@ -274,8 +279,6 @@ kbds_isdelim(Line line, int x, int y, int xoff, int len, wchar_t *delims)
 void
 kbds_nextword(int start, int dir, wchar_t *delims)
 {
-	int top = IS_SET(MODE_ALTSCREEN) ? 0 : -term.histf + term.scr;
-	int bot = IS_SET(MODE_ALTSCREEN) ? term.row-1 : term.row + term.scr - 1;
 	int x = kbds_c.x, y = kbds_c.y;
 	Line line = TLINE(y);
 	int len = tlinelen(line);
@@ -288,7 +291,7 @@ kbds_nextword(int start, int dir, wchar_t *delims)
 		x += dir;
 		if (x < 0 || x >= len) {
 			y += dir;
-			if (y < top || y > bot)
+			if (y < kbds_top() || y > kbds_bot())
 				break;
 			line = TLINE(y);
 			len = tlinelen(line);
@@ -304,8 +307,6 @@ kbds_nextword(int start, int dir, wchar_t *delims)
 			else if (y >= term.row)
 				kscrolldown(&((Arg){ .i = y - term.row + 1 }));
 			LIMIT(y, 0, term.row-1);
-			if (!IS_SET(MODE_ALTSCREEN))
-				top = -term.histf + term.scr, bot =  term.row + term.scr - 1;
 			kbds_c.x = x, kbds_c.y = y;
 			kbds_quant--;
 		}
@@ -361,7 +362,8 @@ kbds_pasteintosearch(const char *data, int len, int append)
 int
 kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 {
-	int i, q, *xy, dy, prevscr, count;
+	int i, q, *xy, dy, eol, islast, prevscr, count, wrap;
+	int alt = IS_SET(MODE_ALTSCREEN);
 	Line line;
 	Rune u;
 
@@ -510,7 +512,18 @@ kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 		break;
 	case XK_dollar:
 	case XK_A:
-		kbds_moveto(tlinelen(TLINE(kbds_c.y))-1, kbds_c.y);
+		line = TLINE(kbds_c.y);
+		eol = tlinelen(line)-1;
+		islast = (kbds_c.x == eol || (kbds_c.x == eol-1 && line[eol-1].mode & ATTR_WIDE));
+		if (islast && line[eol].mode & ATTR_WRAP && kbds_c.y < kbds_bot()) {
+			if (++kbds_c.y >= term.row) {
+				kscrolldown(&((Arg){ .i = kbds_c.y - term.row + 1 }));
+				kbds_c.y = term.row-1;
+			}
+			kbds_moveto(tlinelen(TLINE(kbds_c.y))-1, kbds_c.y);
+			break;
+		}
+		kbds_moveto(islast ? term.col-1 : eol, kbds_c.y);
 		break;
 	case XK_asciicircum:
 	case XK_I:
@@ -522,40 +535,37 @@ kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 		break;
 	case XK_End:
 	case XK_KP_End:
-		kbds_c.y = term.row-1;
+		kbds_moveto(kbds_c.x, term.row-1);
 		break;
 	case XK_Home:
 	case XK_KP_Home:
 	case XK_H:
-		kbds_c.y = 0;
+		kbds_moveto(kbds_c.x, 0);
 		break;
 	case XK_M:
-		kbds_c.y = IS_SET(MODE_ALTSCREEN)
-			? (term.row-1) / 2
-			: MIN(term.c.y + term.scr, term.row-1) / 2;
+		kbds_moveto(kbds_c.x, alt ? (term.row-1) / 2
+                                  : MIN(term.c.y + term.scr, term.row-1) / 2);
 		break;
 	case XK_L:
-		kbds_c.y = IS_SET(MODE_ALTSCREEN)
-			? term.row-1
-			: MIN(term.c.y + term.scr, term.row-1);
+		kbds_moveto(kbds_c.x, alt ? term.row-1
+		                          : MIN(term.c.y + term.scr, term.row-1));
 		break;
 	case XK_Page_Up:
 	case XK_KP_Page_Up:
 	case XK_K:
 		prevscr = term.scr;
 		kscrollup(&((Arg){ .i = term.row }));
-		kbds_c.y = IS_SET(MODE_ALTSCREEN)
-			? 0
-			: MAX(0, kbds_c.y - term.row + term.scr - prevscr);
+		kbds_moveto(kbds_c.x, alt ? 0
+		                          : MAX(0, kbds_c.y - term.row + term.scr - prevscr));
 		break;
 	case XK_Page_Down:
 	case XK_KP_Page_Down:
 	case XK_J:
 		prevscr = term.scr;
 		kscrolldown(&((Arg){ .i = term.row }));
-		kbds_c.y = IS_SET(MODE_ALTSCREEN)
-			? term.row-1
-			: MIN(MIN(term.c.y + term.scr, term.row-1), kbds_c.y + term.row + term.scr - prevscr);
+		kbds_moveto(kbds_c.x, alt ? term.row-1
+		                          : MIN(MIN(term.c.y + term.scr, term.row-1),
+		                                    kbds_c.y + term.row + term.scr - prevscr));
 		break;
 	case XK_asterisk:
 	case XK_KP_Multiply:
@@ -563,11 +573,11 @@ kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 		break;
 	case XK_g:
 		kscrollup(&((Arg){ .i = term.histf }));
-		kbds_c.y = 0;
+		kbds_moveto(kbds_c.x, 0);
 		break;
 	case XK_G:
 		kscrolldown(&((Arg){ .i = term.histf }));
-		kbds_c.y = IS_SET(MODE_ALTSCREEN) ? term.row-1 : term.c.y;
+		kbds_moveto(kbds_c.x, alt ? term.row-1 : term.c.y);
 		break;
 	case XK_b:
 	case XK_B:
@@ -626,10 +636,32 @@ kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 			else if (kbds_c.y >= term.row)
 				kscrolldown(&((Arg){ .i = kbds_c.y - term.row + 1 }));
 		} else {
-			for (line = TLINE(kbds_c.y); kbds_quant > 0;) {
+			line = TLINE(kbds_c.y);
+			len = tlinelen(line);
+			while (kbds_quant > 0) {
 				kbds_c.x += (i & 2 ? 1 : -1);
-				if (kbds_c.x < 0 || kbds_c.x >= term.col)
-					break;
+				wrap = (i & 2) && kbds_c.x == len && len > 0 && (line[len-1].mode & ATTR_WRAP);
+				if (wrap || kbds_c.x >= term.col) {
+					if (kbds_c.y >= kbds_bot())
+						break;
+					if (++kbds_c.y >= term.row) {
+						kscrolldown(&((Arg){ .i = kbds_c.y - term.row + 1 }));
+						kbds_c.y = term.row-1;
+					}
+					line = TLINE(kbds_c.y);
+					len = tlinelen(line);
+					kbds_c.x = 0;
+				} else if (kbds_c.x < 0) {
+					if (kbds_c.y <= kbds_top())
+						break;
+					if (--kbds_c.y < 0) {
+						kscrollup(&((Arg){ .i = -kbds_c.y }));
+						kbds_c.y = 0;
+					}
+					line = TLINE(kbds_c.y);
+					len = tlinelen(line);
+					kbds_c.x = (len > 0 && (line[len-1].mode & ATTR_WRAP)) ? len-1 : term.col-1;
+				}
 				if (line[kbds_c.x].mode & ATTR_WDUMMY)
 					continue;
 				kbds_quant--;
