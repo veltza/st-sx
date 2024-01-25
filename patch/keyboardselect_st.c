0594c3bb392f271cfa7615d2f2960c640242bf24
veltza <106755522+veltza@users.noreply.github.com>
2024-01-25 16:17:02 +0200

    keyboard select: refactor the code

--- patch/keyboardselect_st.c
+++ patch/keyboardselect_st.c
@@ -1,14 +1,5 @@
 #include <wctype.h>
 
-static int kbds_in_use, kbds_quant;
-static int kbds_seltype = SEL_REGULAR;
-static int kbds_mode, kbds_directsearch;
-static int kbds_searchlen, kbds_searchdir, kbds_searchcase;
-static int kbds_finddir, kbds_findtill;
-static Glyph *kbds_searchstr;
-static Rune kbds_findchar;
-static TCursor kbds_c, kbds_oc;
-
 enum keyboardselect_mode {
 	KBDS_MODE_MOVE    = 0,
 	KBDS_MODE_SELECT  = 1<<1,
@@ -16,6 +7,28 @@ enum keyboardselect_mode {
 	KBDS_MODE_FIND    = 1<<3,
 	KBDS_MODE_SEARCH  = 1<<4,
 };
+
+enum cursor_wrap {
+	KBDS_WRAP_NONE    = 0,
+	KBDS_WRAP_LINE    = 1<<0,
+	KBDS_WRAP_EDGE    = 1<<1,
+};
+
+typedef struct {
+	int x;
+	int y;
+	Line line;
+	int len;
+} KCursor;
+
+static int kbds_in_use, kbds_quant;
+static int kbds_seltype = SEL_REGULAR;
+static int kbds_mode, kbds_directsearch;
+static int kbds_searchlen, kbds_searchdir, kbds_searchcase;
+static int kbds_finddir, kbds_findtill;
+static Glyph *kbds_searchstr;
+static Rune kbds_findchar;
+static KCursor kbds_c, kbds_oc;
 
 void
 kbds_drawstatusbar(int y)
@@ -74,316 +87,6 @@ kbds_drawstatusbar(int y)
 }
 
 void
-kbds_setmode(int mode)
-{
-	kbds_mode = mode;
-	term.dirty[0] = 1;
-}
-
-void
-kbds_selecttext(void)
-{
-	if (kbds_isselectmode()) {
-		if (kbds_mode & KBDS_MODE_LSELECT)
-			selextend(term.col-1, kbds_c.y, SEL_RECTANGULAR, 0);
-		else
-			selextend(kbds_c.x, kbds_c.y, kbds_seltype, 0);
-		if (sel.mode == SEL_IDLE)
-			kbds_setmode(kbds_mode & ~(KBDS_MODE_SELECT | KBDS_MODE_LSELECT));
-	}
-}
-
-int
-kbds_drawcursor(void)
-{
-	if (kbds_in_use && (!kbds_issearchmode() || kbds_c.y != term.row-1)) {
-		xdrawcursor(kbds_c.x, kbds_c.y, TLINE(kbds_c.y)[kbds_c.x],
-					kbds_oc.x, kbds_oc.y, TLINE(kbds_oc.y)[kbds_oc.x],
-					TLINE(kbds_oc.y), term.col);
-		kbds_oc = kbds_c;
-	}
-	return term.scr != 0 || kbds_in_use;
-}
-
-int
-kbds_moveto(int x, int y)
-{
-	kbds_c.x = x;
-	kbds_c.y = y;
-	LIMIT(kbds_c.x, 0, term.col-1);
-	LIMIT(kbds_c.y, 0, term.row-1);
-	if ((TLINE(kbds_c.y)[kbds_c.x].mode & ATTR_WDUMMY) && kbds_c.x > 0)
-		kbds_c.x--;
-}
-
-void
-kbds_copytoclipboard(void)
-{
-	if (kbds_mode & KBDS_MODE_LSELECT) {
-		selextend(term.col-1, kbds_c.y, SEL_RECTANGULAR, 1);
-		sel.type = SEL_REGULAR;
-	} else {
-		selextend(kbds_c.x, kbds_c.y, kbds_seltype, 1);
-	}
-	xsetsel(getsel());
-}
-
-int
-kbds_top(void)
-{
-	return IS_SET(MODE_ALTSCREEN) ? 0 : -term.histf + term.scr;
-}
-
-int
-kbds_bot(void)
-{
-	return IS_SET(MODE_ALTSCREEN) ? term.row-1 : term.row-1 + term.scr;
-}
-
-int
-kbds_ismatch(Line line, int x, int y, int len)
-{
-	int i, xo = x, yo = y, hlen = len;
-	Line hline = line;
-
-	if (len <= 0 || (x + kbds_searchlen > len &&
-	    (!(line[len-1].mode & ATTR_WRAP) || y >= kbds_bot())))
-		return 0;
-
-	for (i = 0; i < kbds_searchlen; i++, x++) {
-		if (x >= len) {
-			if (!(line[len-1].mode & ATTR_WRAP) || ++y > kbds_bot())
-				return 0;
-			x = 0;
-			line = TLINE(y);
-			if ((len = tlinelen(line)) <= 0)
-				return 0;
-		}
-		if ((kbds_searchcase && kbds_searchstr[i].u != line[x].u) ||
-		    (!kbds_searchcase && kbds_searchstr[i].u != towlower(line[x].u)))
-			return 0;
-	}
-
-	for (i = 0; i < kbds_searchlen; i++, xo++) {
-		if (xo >= hlen) {
-			xo = 0, yo++;
-			hline = TLINE(yo);
-			hlen = tlinelen(hline);
-		}
-		hline[xo].mode |= ATTR_HIGHLIGHT;
-	}
-	return 1;
-}
-
-int
-kbds_searchall(void)
-{
-	int x, y, len, count = 0;
-	Line line;
-
-	if (!kbds_searchlen)
-		return 0;
-
-	for (y = kbds_top(); y <= kbds_bot(); y++) {
-		line = TLINE(y);
-		len = tlinelen(line);
-		for (x = 0; x < len; x++)
-			count += kbds_ismatch(line, x, y, len);
-	}
-	tfulldirt();
-
-	return count;
-}
-
-void
-kbds_searchnext(int dir)
-{
-	int xo, yo, x = kbds_c.x, y = kbds_c.y;
-	Line line = TLINE(y);
-	int len = tlinelen(line);
-	int wrapped = 0;
-
-	if (!kbds_searchlen)
-		goto end;
-
-	if (dir < 0 && x > len)
-		x = len;
-
-	for (kbds_quant = MAX(kbds_quant, 1); kbds_quant > 0;) {
-		for (xo = x, yo = y;;) {
-			x += dir;
-			if (x < 0 || x >= len) {
-				y += dir;
-				if (y < kbds_top())
-					y = kbds_bot(), wrapped++;
-				else if (y > kbds_bot())
-					y = kbds_top(), wrapped++;
-				if (wrapped > 1)
-					goto end;
-				line = TLINE(y);
-				len = tlinelen(line);
-				x = (dir > 0) ? 0 : MAX(len-1, 0);
-			}
-			if (kbds_ismatch(line, x, y, len)) {
-				if (y < 0)
-					kscrollup(&((Arg){ .i = -y }));
-				else if (y >= term.row)
-					kscrolldown(&((Arg){ .i = y - term.row + 1 }));
-				LIMIT(y, 0, term.row-1);
-				kbds_c.x = x, kbds_c.y = y;
-				kbds_quant--;
-				break;
-			}
-			if ((wrapped && dir > 0 && x >= xo && y >= yo) ||
-			    (wrapped && dir < 0 && x <= xo && y <= yo))
-				goto end;
-		}
-	}
-end:
-	kbds_quant = 0;
-}
-
-void
-kbds_findnext(int dir, int repeat)
-{
-	int x = kbds_c.x, y = kbds_c.y;
-	int ox = kbds_c.x, oy = kbds_c.y;
-	int prevx = kbds_c.x, prevy = kbds_c.y;
-	Line line = TLINE(y);
-	int len = tlinelen(line);
-	int skipfirst;
-
-	if (len <= 0 || kbds_findchar == 0)
-		goto end;
-
-	if (dir < 0 && x > len)
-		x = prevx = len;
-
-	kbds_quant = MAX(kbds_quant, 1);
-	skipfirst = (kbds_quant == 1 && repeat && kbds_findtill);
-
-	while (kbds_quant > 0) {
-		x += dir;
-		if (dir > 0 && x >= len) {
-			if (!(line[len-1].mode & ATTR_WRAP) || ++y > kbds_bot())
-				break;
-			line = TLINE(y);
-			if ((len = tlinelen(line)) <= 0)
-				break;
-			x = 0;
-		} else if (x < 0) {
-			if (--y < kbds_top())
-				break;
-			line = TLINE(y);
-			len = tlinelen(line);
-			if (len <= 0 || !(line[len-1].mode & ATTR_WRAP))
-				break;
-			x = len-1;
-		}
-		if (line[x].mode & ATTR_WDUMMY)
-			continue;
-		if (line[x].u == kbds_findchar) {
-			kbds_c.x = kbds_findtill ? prevx : x;
-			kbds_c.y = kbds_findtill ? prevy : y;
-			if (y < 0) {
-				kscrollup(&((Arg){ .i = -y }));
-				kbds_c.y = kbds_findtill ? prevy - y : 0;
-			} else if (y >= term.row) {
-				kscrolldown(&((Arg){ .i = y - term.row + 1 }));
-				kbds_c.y = term.row-1 - (kbds_findtill ? y - prevy : 0);
-			}
-			LIMIT(y, 0, term.row-1);
-			if (skipfirst && ox == kbds_c.x && oy == kbds_c.y)
-				skipfirst = 0;
-			else
-				kbds_quant--;
-		}
-		prevx = x, prevy = y;
-	}
-end:
-	kbds_quant = 0;
-}
-
-void
-kbds_clearhighlights(void)
-{
-	int x, y;
-	Line line;
-
-	for (y = (IS_SET(MODE_ALTSCREEN) ? 0 : -term.histf); y < term.row; y++) {
-		line = TLINEABS(y);
-		for (x = 0; x < term.col; x++)
-			line[x].mode &= ~ATTR_HIGHLIGHT;
-	}
-	tfulldirt();
-}
-
-int
-kbds_isdelim(Line line, int x, int y, int xoff, int len, wchar_t *delims)
-{
-	for (;;) {
-		x += xoff;
-		if (x >= len) {
-			if (len <= 0 || !(line[len-1].mode & ATTR_WRAP) || ++y > kbds_bot())
-				return 1;
-			line = TLINE(y);
-			len = tlinelen(line);
-			x = 0;
-		} else if (x < 0) {
-			if (--y < kbds_top())
-				return 1;
-			line = TLINE(y);
-			len = tlinelen(line);
-			x = len-1;
-			if (x < 0 || !(line[x].mode & ATTR_WRAP))
-				return 1;
-		}
-		if ((line[x].mode & ATTR_WDUMMY) && xoff != 0)
-			continue;
-		return !(line[x].mode & ATTR_SET) || line[x].u == 0 ||
-		       wcschr(delims, line[x].u) != NULL;
-	}
-}
-
-void
-kbds_nextword(int start, int dir, wchar_t *delims)
-{
-	int x = kbds_c.x, y = kbds_c.y;
-	Line line = TLINE(y);
-	int len = tlinelen(line);
-	int xoff = start ? -1 : 1;
-
-	if (dir < 0 && x > len)
-		x = len;
-
-	for (kbds_quant = MAX(kbds_quant, 1); kbds_quant > 0;) {
-		x += dir;
-		if (x < 0 || x >= len) {
-			y += dir;
-			if (y < kbds_top() || y > kbds_bot())
-				break;
-			line = TLINE(y);
-			len = tlinelen(line);
-			x = (dir > 0) ? 0 : MAX(len-1, 0);
-		}
-		if (line[x].mode & ATTR_WDUMMY)
-			continue;
-		if (!kbds_isdelim(line, x, y, 0, len, delims) &&
-		    kbds_isdelim(line, x, y, xoff, len, delims))
-		{
-			if (y < 0)
-				kscrollup(&((Arg){ .i = -y }));
-			else if (y >= term.row)
-				kscrolldown(&((Arg){ .i = y - term.row + 1 }));
-			LIMIT(y, 0, term.row-1);
-			kbds_c.x = x, kbds_c.y = y;
-			kbds_quant--;
-		}
-	}
-	kbds_quant = 0;
-}
-
-void
 kbds_pasteintosearch(const char *data, int len, int append)
 {
 	static char buf[BUFSIZ];
@@ -414,7 +117,7 @@ kbds_pasteintosearch(const char *data, int len, int append)
 				if (wcwidth(u) > 1) {
 					kbds_searchstr[kbds_searchlen-1].mode = ATTR_WIDE;
 					if (kbds_searchlen < term.col-2) {
-						kbds_searchstr[kbds_searchlen].u = '\0';
+						kbds_searchstr[kbds_searchlen].u = 0;
 						kbds_searchstr[kbds_searchlen++].mode = ATTR_WDUMMY;
 					}
 				}
@@ -429,9 +132,320 @@ kbds_pasteintosearch(const char *data, int len, int append)
 }
 
 int
+kbds_top(void)
+{
+	return IS_SET(MODE_ALTSCREEN) ? 0 : -term.histf + term.scr;
+}
+
+int
+kbds_bot(void)
+{
+	return IS_SET(MODE_ALTSCREEN) ? term.row-1 : term.row-1 + term.scr;
+}
+
+int
+kbds_iswrapped(KCursor *c)
+{
+    return c->len > 0 && (c->line[c->len-1].mode & ATTR_WRAP);
+}
+
+int
+kbds_isselectmode(void)
+{
+	return kbds_in_use && (kbds_mode & (KBDS_MODE_SELECT | KBDS_MODE_LSELECT));
+}
+
+int
+kbds_issearchmode(void)
+{
+	return kbds_in_use && (kbds_mode & KBDS_MODE_SEARCH);
+}
+
+void
+kbds_setmode(int mode)
+{
+	kbds_mode = mode;
+	term.dirty[0] = 1;
+}
+
+void
+kbds_selecttext(void)
+{
+	if (kbds_isselectmode()) {
+		if (kbds_mode & KBDS_MODE_LSELECT)
+			selextend(term.col-1, kbds_c.y, SEL_RECTANGULAR, 0);
+		else
+			selextend(kbds_c.x, kbds_c.y, kbds_seltype, 0);
+		if (sel.mode == SEL_IDLE)
+			kbds_setmode(kbds_mode & ~(KBDS_MODE_SELECT | KBDS_MODE_LSELECT));
+	}
+}
+
+void
+kbds_copytoclipboard(void)
+{
+	if (kbds_mode & KBDS_MODE_LSELECT) {
+		selextend(term.col-1, kbds_c.y, SEL_RECTANGULAR, 1);
+		sel.type = SEL_REGULAR;
+	} else {
+		selextend(kbds_c.x, kbds_c.y, kbds_seltype, 1);
+	}
+	xsetsel(getsel());
+}
+
+void
+kbds_clearhighlights(void)
+{
+	int x, y;
+	Line line;
+
+	for (y = (IS_SET(MODE_ALTSCREEN) ? 0 : -term.histf); y < term.row; y++) {
+		line = TLINEABS(y);
+		for (x = 0; x < term.col; x++)
+			line[x].mode &= ~ATTR_HIGHLIGHT;
+	}
+	tfulldirt();
+}
+
+int
+kbds_moveto(int x, int y)
+{
+	if (y < 0)
+		kscrollup(&((Arg){ .i = -y }));
+	else if (y >= term.row)
+		kscrolldown(&((Arg){ .i = y - term.row + 1 }));
+	kbds_c.x = (x < 0) ? 0 : (x > term.col-1) ? term.col-1 : x;
+	kbds_c.y = (y < 0) ? 0 : (y > term.row-1) ? term.row-1 : y;
+	kbds_c.line = TLINE(kbds_c.y);
+	kbds_c.len = tlinelen(kbds_c.line);
+	if (kbds_c.x > 0 && (kbds_c.line[kbds_c.x].mode & ATTR_WDUMMY))
+		kbds_c.x--;
+}
+
+int
+kbds_moveforward(KCursor *c, int dx, int wrap)
+{
+	KCursor n = *c;
+
+	n.x += dx;
+	if (n.x >= 0 && n.x < term.col && (n.line[n.x].mode & ATTR_WDUMMY))
+		n.x += dx;
+
+	if (n.x < 0) {
+		if (!wrap || --n.y < kbds_top())
+			return 0;
+		n.line = TLINE(n.y);
+		n.len = tlinelen(n.line);
+		if ((wrap & KBDS_WRAP_LINE) && kbds_iswrapped(&n))
+			n.x = n.len-1;
+		else if (wrap & KBDS_WRAP_EDGE)
+			n.x = term.col-1;
+		else
+			return 0;
+		n.x -= (n.x > 0 && (n.line[n.x].mode & ATTR_WDUMMY)) ? 1 : 0;
+	} else if (n.x >= term.col) {
+		if (((wrap & KBDS_WRAP_EDGE) ||
+		    ((wrap & KBDS_WRAP_LINE) && kbds_iswrapped(&n))) && ++n.y <= kbds_bot()) {
+			n.line = TLINE(n.y);
+			n.len = tlinelen(n.line);
+			n.x = 0;
+		} else {
+			return 0;
+		}
+	} else if (n.x >= n.len && dx > 0 && (wrap & KBDS_WRAP_LINE)) {
+		if (n.x == n.len && kbds_iswrapped(&n) && n.y < kbds_bot()) {
+			++n.y;
+			n.line = TLINE(n.y);
+			n.len = tlinelen(n.line);
+			n.x = 0;
+		} else if (!(wrap & KBDS_WRAP_EDGE)) {
+			return 0;
+		}
+	}
+	*c = n;
+	return 1;
+}
+
+int
+kbds_ismatch(KCursor c)
+{
+	KCursor m = c;
+	int i, next;
+
+	if (c.x + kbds_searchlen > c.len && (!kbds_iswrapped(&c) || c.y >= kbds_bot()))
+		return 0;
+
+	for (next = 0, i = 0; i < kbds_searchlen; i++) {
+		if (kbds_searchstr[i].mode & ATTR_WDUMMY)
+			continue;
+		if ((next++ && !kbds_moveforward(&c, 1, KBDS_WRAP_LINE)) ||
+		    (kbds_searchcase && kbds_searchstr[i].u != c.line[c.x].u) ||
+		    (!kbds_searchcase && kbds_searchstr[i].u != towlower(c.line[c.x].u)))
+			return 0;
+	}
+
+	for (i = 0; i < kbds_searchlen; i++) {
+		if (!(kbds_searchstr[i].mode & ATTR_WDUMMY)) {
+			m.line[m.x].mode |= ATTR_HIGHLIGHT;
+			kbds_moveforward(&m, 1, KBDS_WRAP_LINE);
+		}
+	}
+	return 1;
+}
+
+int
+kbds_searchall(void)
+{
+	KCursor c;
+	int count = 0;
+
+	if (!kbds_searchlen)
+		return 0;
+
+	for (c.y = kbds_top(); c.y <= kbds_bot(); c.y++) {
+		c.line = TLINE(c.y);
+		c.len = tlinelen(c.line);
+		for (c.x = 0; c.x < c.len; c.x++)
+			count += kbds_ismatch(c);
+	}
+	tfulldirt();
+
+	return count;
+}
+
+void
+kbds_searchnext(int dir)
+{
+	KCursor c = kbds_c, n = kbds_c;
+	int wrapped = 0;
+
+	if (!kbds_searchlen) {
+		kbds_quant = 0;
+		return;
+	}
+
+	if (dir < 0 && c.x > c.len)
+		c.x = c.len;
+
+	for (kbds_quant = MAX(kbds_quant, 1); kbds_quant > 0;) {
+		if (!kbds_moveforward(&c, dir, KBDS_WRAP_LINE)) {
+			c.y += dir;
+			if (c.y < kbds_top())
+				c.y = kbds_bot(), wrapped++;
+			else if (c.y > kbds_bot())
+				c.y = kbds_top(), wrapped++;
+			if (wrapped > 1)
+				break;;
+			c.line = TLINE(c.y);
+			c.len = tlinelen(c.line);
+			c.x = (dir < 0 && c.len > 0) ? c.len-1 : 0;
+			c.x -= (c.x > 0 && (c.line[c.x].mode & ATTR_WDUMMY)) ? 1 : 0;
+		}
+		if (kbds_ismatch(c)) {
+			n = c;
+			kbds_quant--;
+		}
+	}
+
+	kbds_moveto(n.x, n.y);
+	kbds_quant = 0;
+}
+
+void
+kbds_findnext(int dir, int repeat)
+{
+	KCursor prev, c = kbds_c, n = kbds_c;
+	int skipfirst, yoff = 0;
+
+	if (c.len <= 0 || kbds_findchar == 0) {
+		kbds_quant = 0;
+		return;
+	}
+
+	if (dir < 0 && c.x > c.len)
+		c.x = c.len;
+
+	kbds_quant = MAX(kbds_quant, 1);
+	skipfirst = (kbds_quant == 1 && repeat && kbds_findtill);
+
+	while (kbds_quant > 0) {
+		prev = c;
+		if (!kbds_moveforward(&c, dir, KBDS_WRAP_LINE))
+			break;
+		if (c.line[c.x].u == kbds_findchar) {
+			if (skipfirst && prev.x == kbds_c.x && prev.y == kbds_c.y) {
+				skipfirst = 0;
+				continue;
+			}
+			n.x = kbds_findtill ? prev.x : c.x;
+			n.y = c.y;
+			yoff = kbds_findtill ? prev.y - c.y : 0;
+			kbds_quant--;
+		}
+	}
+
+	kbds_moveto(n.x, n.y);
+	kbds_moveto(kbds_c.x, kbds_c.y + yoff);
+	kbds_quant = 0;
+}
+
+int
+kbds_isdelim(KCursor c, int xoff, wchar_t *delims)
+{
+	if (xoff && !kbds_moveforward(&c, xoff, KBDS_WRAP_LINE))
+		return 1;
+	return wcschr(delims, c.line[c.x].u) != NULL;
+}
+
+void
+kbds_nextword(int start, int dir, wchar_t *delims)
+{
+	KCursor c = kbds_c, n = kbds_c;
+	int xoff = start ? -1 : 1;
+
+	if (dir < 0 && c.x > c.len)
+		c.x = c.len;
+	else if (dir > 0 && c.x >= c.len && c.len > 0)
+		c.x = c.len-1;
+
+	for (kbds_quant = MAX(kbds_quant, 1); kbds_quant > 0;) {
+		if (!kbds_moveforward(&c, dir, KBDS_WRAP_LINE)) {
+			c.y += dir;
+			if (c.y < kbds_top() || c.y > kbds_bot())
+				break;
+			c.line = TLINE(c.y);
+			c.len = tlinelen(c.line);
+			c.x = (dir < 0 && c.len > 0) ? c.len-1 : 0;
+			c.x -= (c.x > 0 && (c.line[c.x].mode & ATTR_WDUMMY)) ? 1 : 0;
+		}
+		if (c.len > 0 &&
+		    !kbds_isdelim(c, 0, delims) && kbds_isdelim(c, xoff, delims)) {
+			n = c;
+			kbds_quant--;
+		}
+	}
+
+	kbds_moveto(n.x, n.y);
+	kbds_quant = 0;
+}
+
+int
+kbds_drawcursor(void)
+{
+	if (kbds_in_use && (!kbds_issearchmode() || kbds_c.y != term.row-1)) {
+		xdrawcursor(kbds_c.x, kbds_c.y, TLINE(kbds_c.y)[kbds_c.x],
+					kbds_oc.x, kbds_oc.y, TLINE(kbds_oc.y)[kbds_oc.x],
+					TLINE(kbds_oc.y), term.col);
+		kbds_moveto(kbds_c.x, kbds_c.y);
+		kbds_oc = kbds_c;
+	}
+	return term.scr != 0 || kbds_in_use;
+}
+
+int
 kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 {
-	int i, q, *xy, dy, eol, islast, prevscr, count, wrap;
+	int i, q, dy, eol, islast, prevscr, count, wrap;
 	int alt = IS_SET(MODE_ALTSCREEN);
 	Line line;
 	Rune u;
@@ -471,7 +485,7 @@ kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 				if (wcwidth(u) > 1) {
 					kbds_searchstr[kbds_searchlen-1].mode = ATTR_WIDE;
 					if (kbds_searchlen < term.col-2) {
-						kbds_searchstr[kbds_searchlen].u = '\0';
+						kbds_searchstr[kbds_searchlen].u = 0;
 						kbds_searchstr[kbds_searchlen++].mode = ATTR_WDUMMY;
 					}
 				}
@@ -506,7 +520,8 @@ kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 	case -1:
 		kbds_searchstr = xmalloc(term.col * sizeof(Glyph));
 		kbds_in_use = 1;
-		kbds_c = kbds_oc = term.c;
+		kbds_moveto(term.c.x, term.c.y);
+		kbds_oc = kbds_c;
 		kbds_setmode(KBDS_MODE_MOVE);
 		return MODE_KBDSELECT;
 	case XK_V:
@@ -599,26 +614,19 @@ kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 		break;
 	case XK_dollar:
 	case XK_A:
-		line = TLINE(kbds_c.y);
-		eol = tlinelen(line)-1;
-		islast = (kbds_c.x == eol || (kbds_c.x == eol-1 && line[eol-1].mode & ATTR_WIDE));
-		if (islast && line[eol].mode & ATTR_WRAP && kbds_c.y < kbds_bot()) {
-			if (++kbds_c.y >= term.row) {
-				kscrolldown(&((Arg){ .i = kbds_c.y - term.row + 1 }));
-				kbds_c.y = term.row-1;
-			}
-			kbds_moveto(tlinelen(TLINE(kbds_c.y))-1, kbds_c.y);
-			break;
-		}
-		kbds_moveto(islast ? term.col-1 : eol, kbds_c.y);
+		eol = kbds_c.len-1;
+		line = kbds_c.line;
+		islast = (kbds_c.x == eol || (kbds_c.x == eol-1 && (line[eol-1].mode & ATTR_WIDE)));
+		if (islast && kbds_iswrapped(&kbds_c) && kbds_c.y < kbds_bot())
+			kbds_moveto(tlinelen(TLINE(kbds_c.y+1))-1, kbds_c.y+1);
+		else
+			kbds_moveto(islast ? term.col-1 : eol, kbds_c.y);
 		break;
 	case XK_asciicircum:
 	case XK_I:
-		line = TLINE(kbds_c.y);
-		len = tlinelen(line);
-		for (i = 0; i < len && line[i].u == ' '; i++)
+		for (i = 0; i < kbds_c.len && kbds_c.line[i].u == ' '; i++)
 			;
-		kbds_moveto((i < len) ? i : 0, kbds_c.y);
+		kbds_moveto((i < kbds_c.len) ? i : 0, kbds_c.y);
 		break;
 	case XK_End:
 	case XK_KP_End:
@@ -685,7 +693,7 @@ kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 			kscrollup(&((Arg){ .i = -dy }));
 		else
 			kscrolldown(&((Arg){ .i = dy }));
-		kbds_c.y += term.scr - prevscr;
+		kbds_moveto(kbds_c.x, kbds_c.y + term.scr - prevscr);
 		break;
 	case XK_f:
 	case XK_F:
@@ -706,7 +714,7 @@ kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 	case XK_0:
 	case XK_KP_0:
 		if (!kbds_quant) {
-			kbds_c.x = 0;
+			kbds_moveto(0, kbds_c.y);
 			break;
 		}
 		/* FALLTHROUGH */
@@ -734,40 +742,11 @@ kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 
 		if (i & 1) {
 			kbds_c.y += kbds_quant * (i & 2 ? 1 : -1);
-			if (kbds_c.y < 0)
-				kscrollup(&((Arg){ .i = -kbds_c.y }));
-			else if (kbds_c.y >= term.row)
-				kscrolldown(&((Arg){ .i = kbds_c.y - term.row + 1 }));
 		} else {
-			line = TLINE(kbds_c.y);
-			len = tlinelen(line);
-			while (kbds_quant > 0) {
-				kbds_c.x += (i & 2 ? 1 : -1);
-				wrap = (i & 2) && kbds_c.x == len && len > 0 && (line[len-1].mode & ATTR_WRAP);
-				if (wrap || kbds_c.x >= term.col) {
-					if (kbds_c.y >= kbds_bot())
-						break;
-					if (++kbds_c.y >= term.row) {
-						kscrolldown(&((Arg){ .i = kbds_c.y - term.row + 1 }));
-						kbds_c.y = term.row-1;
-					}
-					line = TLINE(kbds_c.y);
-					len = tlinelen(line);
-					kbds_c.x = 0;
-				} else if (kbds_c.x < 0) {
-					if (kbds_c.y <= kbds_top())
-						break;
-					if (--kbds_c.y < 0) {
-						kscrollup(&((Arg){ .i = -kbds_c.y }));
-						kbds_c.y = 0;
-					}
-					line = TLINE(kbds_c.y);
-					len = tlinelen(line);
-					kbds_c.x = (len > 0 && (line[len-1].mode & ATTR_WRAP)) ? len-1 : term.col-1;
-				}
-				if (line[kbds_c.x].mode & ATTR_WDUMMY)
-					continue;
-				kbds_quant--;
+			for (;kbds_quant > 0; kbds_quant--) {
+				if (!kbds_moveforward(&kbds_c, (i & 2) ? 1 : -1,
+					    KBDS_WRAP_LINE | KBDS_WRAP_EDGE))
+					break;
 			}
 		}
 		kbds_moveto(kbds_c.x, kbds_c.y);
@@ -776,16 +755,4 @@ kbds_keyboardhandler(KeySym ksym, char *buf, int len, int forcequit)
 	kbds_quant = 0;
 	term.dirty[0] = 1;
 	return 0;
-}
-
-int
-kbds_isselectmode(void)
-{
-	return kbds_in_use && (kbds_mode & (KBDS_MODE_SELECT | KBDS_MODE_LSELECT));
-}
-
-int
-kbds_issearchmode(void)
-{
-	return kbds_in_use && (kbds_mode & KBDS_MODE_SEARCH);
 }
